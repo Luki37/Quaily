3c2bcd12b46198ebecb1a5e711aef5cf5dbc67af
Luki37 <286344374+Luki37@users.noreply.github.com>
2026-07-09 16:05:46 +0200

    define basic functions

---
+++ script.js
@@ -0,0 +1,49 @@
+/*Namen für neuen Stall definieren*/
+function setNewCoopName() {}
+
+/*neue html seite erschaffen für neuen Stall*/
+function addNewPage() {}
+
+/*Stall im Dashboard anlegen*/
+function createCoopDashboard() {
+  const coopDash = document.createElement("div");
+  const link = document.createElement("a");
+  const inputBlock = document.createElement("div");
+  const inputField = document.createElement("input");
+}
+
+/*Funktion neuen Stall hinzufügen*/
+function addCoop() {
+  setNewCoopName();
+  addNewPage();
+  createCoopDashboard();
+}
+
+/*Eier hinzufügen*/
+function addEggs() {}
+
+/*Ausmisten zurücksetzen*/
+function clean() {}
+
+/*Füttern zurücksetzen*/
+function feed() {}
+
+/*Licht einschalten zurücksetzen*/
+function lightOn() {}
+
+/*Licht ausschalten zurücksetzen*/
+function lightOff() {}
+
+/*Archiv anzeigen*/
+function archive() {}
+
+/*Funktionen für die Unterseiten*/
+
+/*Wachtel hinzufügen*/
+function plusQuail() {}
+
+/*Wachtel ausstallen*/
+function minusQuail() {}
+
+/*Alles Ausstallen*/
+function emptyCoop() {}
